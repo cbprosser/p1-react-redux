a052bb2ca9321e79a72ec3b453498badd9fe2b3a
cbprosser <52571711+cbprosser@users.noreply.github.com>
2019-08-02 14:00:34 -0400

    buildspec file

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NotFound from './components/not-found/not-found.component';
+import NavComponent from './components/nav/nav.component';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
       <div className="App">
+        <NavComponent/>
         <Switch>
           <Route component={NotFound}/>
         </Switch>
---
+++ src/components/nav/nav.component.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Navbar, NavbarBrand, Button, NavbarToggler, Nav, NavItem, Collapse, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/logo-color.png'
+
+export default class NavComponent extends Component {
+    render() {
+        return (
+            <div>
+                Navbar goes here
+            </div>
+        )
+    }
+}
